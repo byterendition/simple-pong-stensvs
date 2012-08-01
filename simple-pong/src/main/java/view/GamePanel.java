7a9aff62d07756d0ebfebb6d1d99aab33194cdff
rickvs1990@gmail.com <rickvs1990@gmail.com@d5151f15-00bf-8700-8fa1-dda69ed0e5ae>
2012-08-01 17:26:49 +0000

    Added Plankie

--- simple-pong/src/main/java/view/GamePanel.java
+++ simple-pong/src/main/java/view/GamePanel.java
@@ -3,18 +3,46 @@ package view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputAdapter;
 
+@SuppressWarnings("serial")
 public class GamePanel extends JPanel {
+	
+	Point mouseLocation = new Point();
+	BufferedImage offScreenImage;
 	
 	public GamePanel(){
 		setPreferredSize(new Dimension(400, 400));
+		offScreenImage = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
+		setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		addMouseMotionListener(new GamePanelListener());
 	}
 	
 	@Override
 	public void paint(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		
+		gOffScreen.setColor(Color.BLACK);
+		gOffScreen.fillRect(0, 0, getWidth(), getHeight());
+		gOffScreen.setColor(Color.WHITE);
+		gOffScreen.fillRect(mouseLocation.x-20, 385, 40, 5);
+		
+		g.drawImage(offScreenImage, 0, 0, null);
+	}
+	
+	private class GamePanelListener extends MouseInputAdapter{
+		
+		public void mouseMoved(MouseEvent e){
+			if(contains(e.getPoint())){
+				mouseLocation = e.getPoint();
+				repaint();
+			}
+		}
 	}
 }
