a8d1187f5368b26402481a5dd73704824757c46a
finalart2005@gmail.com <finalart2005@gmail.com@d5151f15-00bf-8700-8fa1-dda69ed0e5ae>
2012-08-01 18:34:07 +0000

    RickvS lekker prutsen

---
+++ branches/simple-pong-RickvS/src/main/java/view/GamePanel.java
@@ -0,0 +1,120 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.event.MouseInputAdapter;
+
+@SuppressWarnings("serial")
+public class GamePanel extends JPanel {
+	
+	private boolean	gameStarted		= false;
+	private boolean	lost			= false;
+	
+	private Point	boardLocation	= new Point();
+	final int		boardWidth		= 40;
+	
+	private Point	ballLocation	= new Point();
+	private Point	ballSpeedVector	= new Point();
+	final int		ballSpeed		= 1;
+	final int		ballRadius		= 5;
+	
+	BufferedImage	offScreenImage;
+	
+	Timer			timer;
+	
+	public GamePanel() {
+		setPreferredSize(new Dimension(400, 400));
+		offScreenImage = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
+		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		addMouseListener(new GamePanelListener());
+		addMouseMotionListener(new GamePanelListener());
+		
+		boardLocation = new Point(200, 385);
+		ballLocation = new Point(200, 50);
+		timer = new Timer(5, new TimerListener());
+		timer.start();
+		
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		
+		gOffScreen.setColor(Color.BLACK);
+		gOffScreen.fillRect(0, 0, getWidth(), getHeight());
+		gOffScreen.setColor(Color.WHITE);
+		gOffScreen.fillRect(boardLocation.x - boardWidth / 2, boardLocation.y, boardWidth, 5);
+		
+		if (lost) {
+			gOffScreen.setColor(Color.RED);
+		}
+		gOffScreen.fillOval(ballLocation.x, ballLocation.y, ballRadius * 2, ballRadius * 2);
+		
+		g.drawImage(offScreenImage, 0, 0, null);
+	}
+	
+	private class GamePanelListener extends MouseInputAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (!gameStarted) {
+				gameStarted = true;
+				lost = false;
+				ballLocation = new Point(200, 50);
+				Random r = new Random();
+				int direction = r.nextInt(2) * 2 - 1;
+				ballSpeedVector = new Point(direction * ballSpeed, ballSpeed);
+			}
+		}
+		
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			if (contains(e.getPoint())) {
+				boardLocation.x = e.getPoint().x;
+				repaint();
+			}
+		}
+	}
+	
+	private class TimerListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (gameStarted) {
+				ballLocation.x += ballSpeedVector.x;
+				ballLocation.y += ballSpeedVector.y;
+				
+				if (ballLocation.x < ballRadius || ballLocation.x > getWidth() - ballRadius) {
+					ballSpeedVector.x = -ballSpeedVector.x;
+				}
+				
+				if (ballLocation.y < ballRadius) {
+					ballSpeedVector.y = -ballSpeedVector.y;
+				}
+				
+				if (ballLocation.y > boardLocation.y - ballRadius && ballLocation.y <= boardLocation.y + ballSpeedVector.y && ballLocation.x > boardLocation.x - boardWidth / 2 && ballLocation.x < boardLocation.x + boardWidth / 2) {
+					ballSpeedVector.y = -ballSpeedVector.y;
+					
+					int overshoot = (boardLocation.y - ballRadius) - ballLocation.y;
+					ballLocation.y += 2 * overshoot;
+				}
+				
+				if (ballLocation.y + ballSpeedVector.y > 400 - ballRadius) {
+					gameStarted = false;
+					lost = true;
+				}
+				
+				repaint();
+			}
+		}
+	}
+}
---
+++ branches/simple-pong-RickvS/src/main/java/view/MainWindow.java
@@ -0,0 +1,58 @@
+package view;
+
+import java.awt.GridBagLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import model.Game;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("serial")
+public class MainWindow extends JFrame {
+	private static final Logger	log	= LoggerFactory.getLogger(MainWindow.class);
+	
+	public MainWindow(Game game) {
+		createAndShowGUI();
+		// TODO Auto-generated constructor stub
+	}
+	
+	private void createAndShowGUI() {
+		setTitle("Simple Pong - by RickvS and Sir_ClickALot");
+		
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new MainWindowListener());
+		
+		setResizable(false);
+		
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(new GridBagLayout());
+		GamePanel gamePanel = new GamePanel();
+		
+		contentPane.add(gamePanel);
+		add(contentPane);
+		
+		pack();
+	}
+	
+	/**
+	 * Listener class which decides what happens if the window is closed
+	 * 
+	 * @author Sir_ClickALot
+	 */
+	private class MainWindowListener extends WindowAdapter {
+		public MainWindowListener() {
+		}
+		
+		@Override
+		public void windowClosing(WindowEvent e) {
+			log.info("Exit");
+			System.exit(0);
+		}
+	}
+}
